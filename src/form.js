75e1626492383e236cb357c58c9361f4dd29f987
João Cruz <jpedrosncruz@gmail.com>
2023-12-22 16:23:16 -0300

    hooks

--- src/form.js
+++ src/form.js
@@ -1,14 +1,23 @@
+import { useRef } from "react";
+import { saveRegister } from "./services/firestore";
+
+let feedbackfield = null;
+let namefield = null;
+
 function FeedbackForm() {
+  feedbackfield = useRef(null);
+  namefield = useRef(null);
+
   return (
-      <form>
+      <form onSubmit={ saveFeedback }>
           <label>
               Envie-nos seu feedback, dúvidas ou sugestões:<br />
-              <textarea cols="50" rows="5"/>
+              <textarea cols="50" rows="5" ref={feedbackfield} />
           </label>
           <br /><br />
           <label>
               Seu contato:<br />
-              <input type="text" />
+              <input type="text" ref={namefield} />
           </label>
           <br /><br />
           <input type="submit" value="Enviar" />
@@ -16,3 +25,11 @@ function FeedbackForm() {
 }
 
 export { FeedbackForm };
+
+function saveFeedback() {
+  const feedback = {
+      message: feedbackfield.current.value,
+      name: namefield.current.value,
+  }
+  saveRegister(feedback);
+}
---
+++ src/services/firestore.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+let firebaseConfig = {
+    'apiKey': process.env.REACT_APP_FIREBASE_API_KEY,
+    'authDomain': process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    'projectId': process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+async function saveRegister (feedback) {
+  await addDoc(collection(db, "feedback"), feedback);
+}
+
+export { saveRegister };
